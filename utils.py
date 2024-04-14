adbf3a3455dc1a30f37f27a07a5605519d78a4dc
pollyjuice74 <104398042+pollyjuice74@users.noreply.github.com>
2024-04-13 22:09:35 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import os
 
 def push_to_git():
     os.system('git config --global user.name "pollyjuice74"')
-    os.system('git remote set-url origin https://pollyjuice74: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@github.com/pollyjuice74/DDECC.git')
+    os.system('git remote set-url origin https://pollyjuice74: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@github.com/pollyjuice74/DDECC.git')
     
     os.system('git add .')
     os.system('git commit -m "Add trained model weights"')
