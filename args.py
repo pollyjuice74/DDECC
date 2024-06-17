f160244089cc6612c6968032b1f1d207b2d1c4bb
pollyjuice74 <104398042+pollyjuice74@users.noreply.github.com>
2024-06-17 17:22:11 -0400

    Update args.py

--- args.py
+++ args.py
@@ -6,7 +6,6 @@ import torch
 import logging
 from datetime import datetime
 
-
 def pass_args_ddecc():
     parser = argparse.ArgumentParser(description='PyTorch DDPM_ECCT')
     parser.add_argument('--epochs', type=int, default=2000)
@@ -31,7 +30,8 @@ def pass_args_ddecc():
     # DDECC args
     parser.add_argument('--sigma', type=float, default=0.01)
 
-    args = parser.parse_args()
+    args = parser.parse_args(args=[])
+    print("%%%%%%%%%%%%%%%")
     os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
     os.environ["CUDA_VISIBLE_DEVICES"] = args.gpus
     set_seed(args.seed)
@@ -43,12 +43,15 @@ def pass_args_ddecc():
     code.k = args.code_k
     code.n = args.code_n
     code.code_type = args.code_type
-    G, H = Get_Generator_and_Parity(code)
-    code.generator_matrix = torch.from_numpy(G).transpose(0, 1).long()
-    code.pc_matrix = torch.from_numpy(H).long()
-    args.code = code
-    ###
-    args.N_steps = code.pc_matrix.shape[0]+5 #number of diffusion steps
+
+    # UPDATE
+    # G, H = Get_Generator_and_Parity(code)
+    # code.generator_matrix = torch.from_numpy(G).transpose(0, 1).long()
+    # code.pc_matrix = torch.from_numpy(H).long() 
+    
+    # args.code = code
+    
+    # args.N_steps = code.pc_matrix.shape[0]+5 #number of diffusion steps #####################
     ####################################################################
     model_dir = os.path.join('DDECCT_Results',
                              args.code_type + '__Code_n_' + str(
@@ -63,7 +66,6 @@ def pass_args_ddecc():
     logging.basicConfig(level=print, format='%(message)s',
                         handlers=handlers)
     print(f"Path to model/logs: {model_dir}")
-    print(args)
+    # print(args)
 
     return args
-
