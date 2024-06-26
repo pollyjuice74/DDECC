a0c247bc73b5e9cd88de8bc569a59af4e30dda40
pollyjuice74 <104398042+pollyjuice74@users.noreply.github.com>
2024-06-25 21:07:54 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -7,17 +7,14 @@ def push_to_git():
     os.system('git add .')
     os.system('git commit -m "Add trained model weights"')
     os.system('git push origin main')
+    
+# ### PUSH TO GITHUB ###
+# !git config --global user.name "pollyjuice74"
+# !git config --global user.email "hernandez.aht82836@gmail.com"
 
+# !git remote set-url origin https://pollyjuice74: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@github.com/pollyjuice74/DDECC.git
 
-
-
-        ### PUSH TO GITHUB ###
-        !git config --global user.name "pollyjuice74"
-        !git config --global user.email "hernandez.aht82836@gmail.com"
-
-        !git remote set-url origin https://pollyjuice74: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@github.com/pollyjuice74/DDECC.git
-
-        !git add .
-        !git commit -m "Add trained model weights"
-        !git push origin main
-        ######################
+# !git add .
+# !git commit -m "Add trained model weights"
+# !git push origin main
+# ######################
