6f0114bf8aefc853284dfd70b0d88bb776989913
pollyjuice74 <104398042+pollyjuice74@users.noreply.github.com>
2024-06-27 16:35:38 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -11,10 +11,10 @@ from torch.utils.data import DataLoader
 from torch.utils import data
 from datetime import datetime
 import logging
-from Codes import *
+from src.codes import *
 import time
 from torch.optim.lr_scheduler import CosineAnnealingLR
-from DDECC import DDECCT
+from src.model import DDECCT
 
 
 
